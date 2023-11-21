757e3cdde334587c8819d3268dab7dc4e70acb62
srinith2710 <21bd1a051xcsea@gmail.com>
2023-11-21 15:12:35 +0530

    First

---
+++ src/test/java/in/kmit/PROJECT_JAVA2/AppTest.java
@@ -0,0 +1,20 @@
+package in.kmit.PROJECT_JAVA2;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
